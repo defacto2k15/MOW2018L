259df6761dc80c2d43ee19cf634344289b6fb230
bartek <bczerwinski462@gmail.com>
2018-04-19 17:23:25 +0200

    Working genetic algorithm

--- mow.R
+++ mow.R
@@ -1,1 +1,74 @@
+library(GA)
 library(cec2017)
+
+goalFunctionId <- 10;
+
+searchRange.x <- c(0, 50)
+searchRange.y <- c(0, 50)
+
+# funkcja celu
+goalFunction <- function(x1, x2)
+{
+  cec2017(goalFunctionId, matrix(
+    data = c(x1, x2),
+    ncol = 2,
+    nrow = length(x1)
+  ))
+}
+
+# rysowanie diagramu pokzaujaca przekrój wartosci
+x1 <- seq(searchRange.x[1], searchRange.x[2], by = 1)
+x2 <- seq(searchRange.y[1], searchRange.y[2], by = 1)
+
+f <- outer(x1,
+           x2,
+           goalFunction)
+
+filled.contour(x1, x2, f, color.palette = bl2gr.colors)
+
+# points zawierac bedzie historie znajdywanych punktów
+points <- list()
+
+ga_maxiter <- 200
+
+GA <- ga(
+  type = "real-valued",
+  fitness =  function(x)
+    goalFunction(x[1], x[2]),
+  min = c(searchRange.x[1], searchRange.y[1]),
+  max = c(searchRange.x[2], searchRange.y[2]),
+  popSize = 50,
+  maxiter = ga_maxiter,
+  run = 100,
+  monitor = function(x) {
+    xs <- x@population[, 1]
+    
+    ys <- x@population[, 2]
+    
+    points[[x@iter]] <<- rbind (xs, ys)
+    gaMonitor(x)
+    
+  }
+)
+
+
+intensity_colors <- terrain.colors(ga_maxiter)
+
+filled.contour(x1, x2, f, color.palette = bl2gr.colors,
+               plot.axes = {
+                 axis(1)
+                 axis(2)
+                 
+                 for (iter in 1:GA@iter) {
+                   points(points[[iter]][1, ], points[[iter]][2, ], col = intensity_colors[iter])
+                 }
+                 
+                 points(
+                   GA@solution[, 1],
+                   GA@solution[, 2],
+                   pch = 3,
+                   cex = 2,
+                   col = "white",
+                   lwd = 2
+                 )
+               })
