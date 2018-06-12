0d56cee83c020f985fbc4f72ae3052b1e48742c3
bartek <bczerwinski462@gmail.com>
2018-06-12 18:28:23 +0200

    Before generalization to manydimensional

--- mow2.R
+++ mow2.R
@@ -37,6 +37,13 @@ config = rev(within(list(), {
     variogram.kappa = 1
   }))
   
+  approximation_integration = rev(within(list(), {
+    use_approximation_fit_percent = T;
+    appriximation_fit_percent = 0.8;
+    
+    use_approximation_population_percent = F;
+    appriximation_population_percent = 0.8;
+  }))
 }))
 # spline regression
 
@@ -47,6 +54,15 @@ goalFunction <- function(x1, x2)
     data = c(x1, x2),
     ncol =2,
     nrow = length(x1)
+  ))
+}
+
+goalFunction_dataFrame <- function(x)
+{
+  cec2017(config$goalFunctionId, matrix(
+    data = as.vector(x),
+    ncol =ncol(x),
+    nrow = nrow(x)
   ))
 }
 
@@ -61,6 +77,12 @@ panel.real = levelplot( ye~ dataPoints.x* dataPoints.y, contour=TRUE, panel = pa
 
 
 approximated_target_function_df <- NULL;
+targetFunction.real = function(x){
+  goalFunction_dataFrame(x);
+};
+targetFunction.approximated = NULL;
+
+
 if(config$approximation_type != "kriging"){
   
   polyLevel <- config$polyLevel;
@@ -75,7 +97,7 @@ if(config$approximation_type != "kriging"){
     if( config$approximation_type == "polynomial"){
       fit <- lm(regressionRelation, y=TRUE, x=TRUE,data=dataPoints)
       predictResults <- predict(fit)
-      approximated_target_function_df <- function(df){
+      targetFunction.approximated <- function(df){
          predict(fit, data.frame(dataPoints.x=df[,1], dataPoints.y=df[,2]))
       }
     }
@@ -171,68 +193,66 @@ ga_migrationInterval <- 10;
 ga_epochCount <- ga_maxiter/ga_migrationInterval;
 
 current_iteration <- 0;
-GA <- my_gaisl(
+GA <- my_ga(
   type = "real-valued",
-  function(x){
-      -approximated_target_function_df(x)
-      @-goalFunction(x[,1], x[,2])
+  fitness =  function(x,a){
+    if(config$approximation_integration$use_approximation_population_percent){
+      if( runif(1) < config$approximation_integration$appriximation_population_percent ){
+        -targetFunction.approximated(x);
+      }else{
+        -targetFunction.real(x);
+      }
+    }else if(config$approximation_integration$use_approximation_fit_percent){
+      perc <- config$approximation_integration$appriximation_fit_percent;
+      splitter <- sample(c(0,1), replace=TRUE, size=nrow(x), prob=c(perc, 1-perc))
+      splitedPoints <- split(x,splitter);
+      
+      results <- list();
+      results$`0` <- -targetFunction.approximated(matrix(splitedPoints$`0`, nrow=sum(splitter==0), ncol=2));
+      results$`1` <- -targetFunction.real(matrix(splitedPoints$`1`, nrow=sum(splitter==1), ncol=2));
+      
+      
+      as.vector(unsplit(results, splitter));
+    }else{
+      -targetFunction.real(x);
+    }
   },
   min = c(searchRange.x[1], searchRange.y[1]),
   max = c(searchRange.x[2], searchRange.y[2]),
   popSize = 50,
   maxiter = ga_maxiter,
-  migrationInterval = ga_migrationInterval,
   run = 100,
-  numIslands = ga_islandCount,
   monitor = function(x) {
-    ru <<- x;
-    current_iteration <<- current_iteration+1;
+    current_iteration <<- x@iter;
+    xs <- x@population[, 1]
     
-    xs <- vector();
-    ys <- vector();
+    ys <- x@population[, 2]
     
-    for(i in 1:ga_islandCount ){ 
-      xs <-  ru@islands[[i]]@population[,1]
-      ys <-  ru@islands[[i]]@population[,2] 
-      points[[i]][[current_iteration]] <<- rbind (xs, ys)
-      
-    }
-    gaislMonitor(x)
+    points[[x@iter]] <<- rbind (xs, ys)
+    gaMonitor(x)
     
   },
-  #crossover = my_crossover,
-  parallel = F
+  crossover = my_crossover_internal
 )
 
-final_epoch_count <-  nrow(GA@summary[[1]])/GA@migrationInterval;
 
-intensity_colors <- list();
-for(island in 1:ga_islandCount){
-  max_col <- (ga_islandCount*2)+2;
-  intensity_colors[[island]] <- rainbow(n=final_epoch_count, start = (2*island+1)/max_col, end=(2*island+2)/max_col);
-}
-terrain.colors(final_epoch_count)
+intensity_colors <- terrain.colors(ga_maxiter)
 
 filled.contour(x1, x2, f, color.palette = bl2gr.colors,
                plot.axes = {
                  axis(1)
                  axis(2)
-
-                 for(island in 1:ga_islandCount){
-
-                   for (iter in 1:final_epoch_count) {
-                     x <- points[[island]][[iter]][1, ] + island;
-                     y <- points[[island]][[iter]][2, ] + island;
-                     points(x, y, col = intensity_colors[[island]][[iter]])
-                   }
-
-                   points(
-                     GA@islands[[island]]@solution[, 1],
-                     GA@islands[[island]]@solution[, 2],
-                     pch = 3,
-                     cex = 2,
-                     col = intensity_colors[[island]][[1]],
-                     lwd = 2
-                   )
+                 
+                 for (iter in 1:GA@iter) {
+                   points(points[[iter]][1, ], points[[iter]][2, ], col = intensity_colors[iter])
                  }
+                 
+                 points(
+                   GA@solution[, 1],
+                   GA@solution[, 2],
+                   pch = 3,
+                   cex = 2,
+                   col = "white",
+                   lwd = 2
+                 )
                })
